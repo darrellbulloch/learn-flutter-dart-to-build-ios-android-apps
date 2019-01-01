d15daf489b3ace2af526245d5ba556a57cee8627
Bulloch <darrell.bulloch@gulfstream.com>
2018-12-31 19:02:25 -0500

    Changes

--- flutter_course/lib/pages/products.dart
+++ flutter_course/lib/pages/products.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 import '../product_manager.dart';
+import '../pages/products_admin.dart';
 
 class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('Choose'),
+            ),
+            ListTile(
+              title: Text('Manage Products'),
+              onTap: () {
+                return Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (BuildContext context) => ProductsAdminPage(),
+                  ),
+                );
+              },
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text('EasyList'),
       ),
